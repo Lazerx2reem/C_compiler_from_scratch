d05804e68a4828e4a9620ca16b95740ccc4bbd18
Arsh Gupta <arsh121523@gmail.com>
2025-02-04 09:29:37 -0500

    Added the functions to work with

--- value.h
+++ value.h
@@ -11,5 +11,9 @@ typedef struct{
 	Value* values;
 } ValueArray;
 
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+
 #endif
 
