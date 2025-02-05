0708377132bef6148cc3c1ef229d3aa36491466e
Arsh Gupta <arsh121523@gmail.com>
2025-02-05 13:45:09 -0500

    Added Array in chunk.c

--- chunk.c
+++ chunk.c
@@ -7,6 +7,7 @@ void initChunk(Chunk* chunk) {
 	chunk->count = 0;
 	chunk->capacity = 0;
 	chunk->code = NULL;
+	initValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte) {
