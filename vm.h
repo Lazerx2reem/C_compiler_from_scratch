2f8af664a1cc438f7d31dcef3631686f09a91ca8
Arsh Gupta <arsh121523@gmail.com>
2025-02-10 13:29:09 -0500

    added definition

--- vm.h
+++ vm.h
@@ -7,6 +7,12 @@ typedef struct {
   Chunk* chunk;
 } VM;
 
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
 void initVM();
 void freeVM();
 InterpretResult interpret(Chunk* chunk);
