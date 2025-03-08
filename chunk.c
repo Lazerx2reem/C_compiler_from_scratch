362f8e292a3f43f10cacc8d8b105dde2ef03c806
Arsh Gupta <arsh121523@gmail.com>
2025-03-08 18:45:48 -0500

    Minor change

--- chunk.c
+++ chunk.c
@@ -18,7 +18,6 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
 	chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
 	chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
 	}
-	
 	chunk->code[chunk->count] = byte;
 	chunk->lines[chunk->count] = line;
 	chunk->count++;
