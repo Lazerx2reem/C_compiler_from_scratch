45234a42994d5be37c7ea666beeef8a6f8ba76af
Arsh Gupta <arsh121523@gmail.com>
2025-03-26 19:13:03 -0400

    Added Binary_OP code

--- vm.c
+++ vm.c
@@ -30,6 +30,12 @@ Value pop(){
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
