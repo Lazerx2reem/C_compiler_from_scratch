335833be65bbf1b79baa075b711320046ea0cdac
Arsh Gupta <arsh121523@gmail.com>
2025-04-11 01:34:42 -0400

    Added case OP_ADD

--- debug.c
+++ debug.c
@@ -39,6 +39,8 @@ int  disassembleInstruction(Chunk* chunk, int offset) {
 	switch (instruction) {
 		case OP_CONSTANT:
 			return constantInstruction("OP_CONSTANT", chunk, offset);
+		case OP_ADD:
+			return simpleInstruction("OP_ADD", offset);
 		case OP_NEGATE:
 			return simpleInstruction("OP_NEGATE", offset);
 		case OP_RETURN:
